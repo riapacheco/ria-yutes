f4ebf99b2b6f7f86ed2b489534a4eeadc9ab2583
Patrrck Kelly <hey@butterflydesin.me>
2020-12-23 16:08:55 -0700

    changed structure for scientific reasons

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+/* eslint-disable header/header */
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+[object Object]
+ */
