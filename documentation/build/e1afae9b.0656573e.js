7a5d8f0621f0b4efc41e9ab3d0f37d0aa7184e92
Patrrck Kelly <hey@butterflydesin.me>
2020-12-23 14:32:24 -0700

    include build folder in docs branch

---
+++ documentation/build/e1afae9b.0656573e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{91:function(s){s.exports=JSON.parse('{"allTagsPath":"/ria-yutes/blog/tags","slug":"docusaurus","name":"docusaurus","count":1,"permalink":"/ria-yutes/blog/tags/docusaurus"}')}}]);
