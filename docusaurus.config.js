56605a64e8498aa973301622e30148f3b7cd47c9
Patrrck Kelly <hey@butterflydesin.me>
2020-12-23 22:35:18 -0700

    added a PWA functionality with offline mode.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,6 +7,23 @@
  * @format
  */
 
+const path = require('path');
+
+const customFields = {
+  ria: {
+    businessUrls: {
+      portfolio: '',
+      steer: 'https://steer.software',
+    },
+    socialUrls: {
+      linkdin: '',
+      github: 'https://github.com/riapacheco',
+      twitter: '',
+    },
+    avatar:
+      'https://avatars2.githubusercontent.com/u/53801436?s=460&u=24a361c7cb532e96ff36122fae9d593c73897066&v=4',
+  },
+};
 module.exports = {
   title: 'Yutes',
   tagline: 'Wanna use some Yutes?',
@@ -17,23 +34,69 @@ module.exports = {
   favicon: 'img/light-logo.png',
   organizationName: 'patkellydesigns', // Usually your GitHub org/user name.
   projectName: 'ria-yutes', // Usually your repo name.
+  plugins: [
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        reloadPopup: '@theme/PwaReloadPopup',
+        swCustom: path.resolve(__dirname, 'src/sw.js'),
 
-  themeConfig: {
-    customFields: {
-      ria: {
-        businessUrls: {
-          portfolio: '',
-          steer: 'https://steer.software',
-        },
-        socialUrls: {
-          linkdin: '',
-          github: 'https://github.com/riapacheco',
-          twitter: '',
-        },
-        avatar:
-          'https://avatars2.githubusercontent.com/u/53801436?s=460&u=24a361c7cb532e96ff36122fae9d593c73897066&v=4',
+        offlineModeActivationStrategies: ['appInstalled', 'queryString'],
+
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/dark-logo.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: 'manifest.json',
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#000',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/dark-logo.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/dark-logo.png',
+            color: 'rgb(37, 194, 160)',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/dark-logo.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#000',
+          },
+        ],
       },
-    },
+    ],
+  ],
+  themeConfig: {
+    customFields,
     navbar: {
       title: 'Yutes',
 
@@ -133,79 +196,6 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        plugins: [
-          [
-            '@docusaurus/plugin-pwa',
-            {
-              injectManifestConfig: {
-                manifestTransforms: [],
-                modifyURLPrefix: {},
-
-                globPatterns: ['**/*.{pdf,docx,xlsx}'],
-              },
-              debug: false,
-              offlineModeActivationStrategies: ['appInstalled', 'queryString'],
-              pwaHead: [
-                {
-                  tagName: 'link',
-                  rel: 'icon',
-                  href: 'img/light-logo.png',
-                },
-                {
-                  tagName: 'link',
-                  rel: 'manifest',
-                  href: 'manifest.json',
-                },
-                {
-                  tagName: 'meta',
-                  name: 'theme-color',
-                  content: 'rgb(37, 194, 160)',
-                },
-              ],
-            },
-          ],
-
-          [
-            '@docusaurus/plugin-content-pages',
-            {
-              /**
-               * Path to data on filesystem
-               * relative to site dir
-               * components in this directory will be automatically converted to pages
-               */
-              path: 'src/pages',
-              /**
-               * URL route for the page section of your site
-               * do not include trailing slash
-               */
-              routeBasePath: '',
-              include: ['**/*.{js,jsx,ts,tsx,md,mdx}'],
-              /**
-               * No Route will be created for matching files
-               */
-              exclude: [
-                '**/_*.{js,jsx,ts,tsx,md,mdx}',
-                '**/*.test.{js,ts}',
-                '**/__tests__/**',
-              ],
-              /**
-               * Theme component used by markdown pages.
-               */
-              mdxPageComponent: '@theme/MDXPage',
-              /**
-               * Remark and Rehype plugins passed to MDX
-               */
-              remarkPlugins: [],
-              rehypePlugins: [],
-              /**
-               * Custom Remark and Rehype plugins passed to MDX before
-               * the default Docusaurus Remark and Rehype plugins.
-               */
-              beforeDefaultRemarkPlugins: [],
-              beforeDefaultRehypePlugins: [],
-            },
-          ],
-        ],
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
 
